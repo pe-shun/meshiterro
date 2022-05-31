89ecbd699d677ccb6a62f07a01a7b6b340a2cd98
EC2 Default User <ec2-user@ip-172-31-89-133.ec2.internal>
2022-05-31 23:06:50 +0000

    meshi

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resources :post_comments, only: [:create]
+
+ end
+
+  resources :users, only: [:show, :edit, :update]
 
 
 end
